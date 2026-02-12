2e1fbedd3b78730d68bebf34a9c8333c8970ba88
Aliza <adkaliza729@gmail.com>
2026-02-12 14:19:54 +0545

    learning express dependencies

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+
+
+const express=require("express");
+const app=express();
+
+const PORT=5000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+app.get("/",(req, res) =>{
+    res.send("Aliza Adhikari")
+
+})
+console.log("Hello World")
